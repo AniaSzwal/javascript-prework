09206a74b4047315e81ab6a42bbdc28d33f799df
Anna Szwal <aniaszwal@gmail.com>
2021-02-15 05:19:28 +0100

    add function: game's results

--- js/script.js
+++ js/script.js
@@ -18,11 +18,11 @@ if(randomNumber == 1) {
     computerMove = 'kamień';
 } else if(randomNumber == 2) {
     computerMove = 'papier';
-} else(randomNumber == 3) {
-    computerMove == 'nożyce';
-
+} else if(randomNumber == 3){
+    computerMove = 'nożyce';
+} else {
+    computerMove = 'błąd'
 }
-
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
@@ -35,11 +35,28 @@ if(playerInput == '1'){
     playerMove = 'kamień';
 } else if(playerInput == '2') {
     playerMove = 'papier';
-} else(playerInput == '3') {
+} else if(playerInput == '3') {
     playerMove = 'kamień';
+} else {
+    playerMove= 'nieznany ruch'
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz');
+}else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}else if(computerMove == playerMove){
+    printMessage('Remis!');
+}else if(computerMove == 'kamień' || "papier" || "nożyce" && playerMove == 'nieznany ruch'){
+    printMessage('Wpisałeś nieznaną wartość! Wybierz cyfrę od 1 do 3');
+}else {
+    printMessage('Błąd! Spróbuj ponownie wpisać wartość z przedziału 1-3')
+}
+
 
 
 
